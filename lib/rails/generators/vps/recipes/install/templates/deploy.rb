793847c99a8ac8e61984c361fbc3c7a173fa55bd
Stephane Busso <stephane.busso@gmail.com>
2012-04-04 16:27:14 +0700

    update deploy.rb

--- lib/rails/generators/vps/recipes/install/templates/deploy.rb
+++ lib/rails/generators/vps/recipes/install/templates/deploy.rb
@@ -8,16 +8,16 @@ set :deploy_to, "/home/#{user}/apps/#{application}"
 set :use_sudo, false
 set :deploy_via, :remote_cache
 set :scm, :git
-set :repository, "git@github.com:activelabs/#{application}.git"
+set :repository, "git@github.com:sbusso/#{application}.git"
 set :git_enable_submodules, 1
 set :keep_releases, 3
 set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-ssh_options[:paranoid] = false
+# ssh_options[:paranoid] = false
 
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
+after "deploy", "deploy:cleanup" # keep only the last 3 releases
 
 # =============================================================================
 # STAGE SETTINGS
@@ -35,9 +35,10 @@ require 'capistrano/ext/multistage'
 require "bundler/capistrano"
 require "capistrano_vps/recipes/base"
 require "capistrano_vps/recipes/nginx"
-# load "capistrano_vps/recipes/unicorn"
+require "capistrano_vps/recipes/unicorn"
 require "capistrano_vps/recipes/postgresql"
 require "capistrano_vps/recipes/nodejs"
-# load "capistrano_vps/recipes/redis"
+require "capistrano_vps/recipes/redis"
 require "capistrano_vps/recipes/rbenv"
-require "capistrano_vps/recipes/check"
+require "capistrano_vps/recipes/libxml"
+# require "capistrano_vps/recipes/check"
