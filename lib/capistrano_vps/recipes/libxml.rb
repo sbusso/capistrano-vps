d3e04d474956c519272a4a3105502a879fd13f66
Stephane Busso <stephane.busso@gmail.com>
2012-04-04 14:19:44 +0700

    integrate libxml

--- lib/capistrano_vps/recipes/libxml.rb
+++ lib/capistrano_vps/recipes/libxml.rb
@@ -1,9 +1,11 @@
-namespace :libxml do
+Capistrano::Configuration.instance(true).load do
+  namespace :libxml do
 
-  desc "Install the latest release of Redis"
-  task :install, roles: :app do
-    run "#{sudo} apt-get install libxml2 libxml2-dev libxslt1-dev"
+    desc "Install the latest release of libxml"
+    task :install, roles: :app do
+      run "#{sudo} apt-get install libxml2 libxml2-dev libxslt1-dev"
+    end
+    after "deploy:install", "libxml:install"
+
   end
-  after "deploy:install", "libxml:install"
 end
-
