dcef547fbda172697028f03d86ad99b8eec1af26
Stephane Busso <stephane.busso@gmail.com>
2012-06-09 16:03:36 +0700

    add wkhtmltoimage

---
+++ lib/capistrano_vps/recipes/wkhtmltoimage.rb
@@ -0,0 +1,16 @@
+Capistrano::Configuration.instance(true).load do
+  namespace :html2image do
+
+    desc "Install the latest release of Imagemagick"
+    task :install, roles: :app do
+
+      run "wget http://wkhtmltopdf.googlecode.com/files/wkhtmltoimage-0.11.0_rc1-static-amd64.tar.bz2 -O wkhtmltoimage.tar.bz2"
+      run "tar xvjf wkhtmltoimage.tar.bz2"
+      run "rm wkhtmltoimage.tar.bz2"
+      run "#{sudo} mv wkhtmltoimage-amd64 /usr/local/bin/"
+
+
+    end
+    after "deploy:install", "html2image:install"
+  end
+end
