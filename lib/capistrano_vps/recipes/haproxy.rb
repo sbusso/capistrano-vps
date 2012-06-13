407578325ec4ee62cf2036e8c2e6bf4d02a9bc7f
Stephane Busso <stephane.busso@gmail.com>
2012-06-13 12:09:42 +0700

    add haproxy with front role

---
+++ lib/capistrano_vps/recipes/haproxy.rb
@@ -0,0 +1,24 @@
+Capistrano::Configuration.instance(true).load do
+  namespace :haproxy do
+    desc "Install latest stable release of haproxy"
+    task :install, roles: :front do
+      run "#{sudo} apt-get -y install haproxy"
+    end
+    after "deploy:install", "haproxy:install"
+
+    desc "Setup haproxy default configuration"
+    task :setup, roles: :front do
+      template "haproxy.erb", "/tmp/haproxy_conf"
+      run "#{sudo} mv /tmp/haproxy_conf /etc/defaut/haproxy"
+      restart
+    end
+    after "deploy:setup", "haproxy:setup"
+
+    %w[start stop restart].each do |command|
+      desc "#{command} haproxy"
+      task command, roles: :front do
+        run "#{sudo} service haproxy #{command}"
+      end
+    end
+  end
+end
