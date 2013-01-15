102b4faca3d5b3925b740711b581c9553fd57326
Stéphane Busso <stephane.busso@gmail.com>
2013-01-15 14:27:24 +0700

    fix whtmltoimage path

--- lib/capistrano_vps/recipes/wkhtmltoimage.rb
+++ lib/capistrano_vps/recipes/wkhtmltoimage.rb
@@ -7,7 +7,7 @@ Capistrano::Configuration.instance(true).load do
       run "wget http://wkhtmltopdf.googlecode.com/files/wkhtmltoimage-0.11.0_rc1-static-amd64.tar.bz2 -O wkhtmltoimage.tar.bz2"
       run "tar xvjf wkhtmltoimage.tar.bz2"
       run "rm wkhtmltoimage.tar.bz2"
-      run "#{sudo} mv wkhtmltoimage-amd64 /usr/local/bin/"
+      run "#{sudo} mv wkhtmltoimage-amd64 /usr/local/bin/wkhtmltoimage"
 
 
     end
