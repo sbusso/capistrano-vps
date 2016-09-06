b34cc454b7a432fff27eda8db747fe2be5007152
Stéphane Busso <sbusso@users.noreply.github.com>
2016-09-06 19:19:25 +0700

    Update wkhtmlimage download url

--- lib/capistrano-vps/recipes/wkhtmltoimage.rb
+++ lib/capistrano-vps/recipes/wkhtmltoimage.rb
@@ -6,7 +6,7 @@ Capistrano::Configuration.instance(true).load do
 
       # sudo aptitude install openssl build-essential xorg libssl-dev
       # sudo aptitude install ia32-libs
-      run "wget https://wkhtmltopdf.googlecode.com/files/wkhtmltoimage-0.11.0_rc1-static-i386.tar.bz2 -O wkhtmltoimage.tar.bz2"
+      run "wget http://download.gna.org/wkhtmltopdf/obsolete/linux/wkhtmltoimage-0.11.0_rc1-static-amd64.tar.bz2 -O wkhtmltoimage.tar.bz2"
       run "tar xvjf wkhtmltoimage.tar.bz2"
       run "rm wkhtmltoimage.tar.bz2"
       run "#{sudo} mv wkhtmltoimage-amd64 /usr/local/bin/wkhtmltoimage"
